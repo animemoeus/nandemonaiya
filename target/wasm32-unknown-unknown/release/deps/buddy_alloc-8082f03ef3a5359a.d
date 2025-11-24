997cab9169c6cf0c2e9d3ef0474a667886433420
Arter Tendean <arter.tendean.07@gmail.com>
2025-11-24 22:54:41 +0700

    Web build

---
+++ target/wasm32-unknown-unknown/release/deps/buddy_alloc-8082f03ef3a5359a.d
@@ -0,0 +1,10 @@
+C:\Users\arter\Documents\Projects\WASM\nandemonaiya\target\wasm32-unknown-unknown\release\deps\libbuddy_alloc-8082f03ef3a5359a.rmeta: C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\lib.rs C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\buddy_alloc.rs C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\fast_alloc.rs C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\non_threadsafe_alloc.rs
+
+C:\Users\arter\Documents\Projects\WASM\nandemonaiya\target\wasm32-unknown-unknown\release\deps\libbuddy_alloc-8082f03ef3a5359a.rlib: C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\lib.rs C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\buddy_alloc.rs C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\fast_alloc.rs C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\non_threadsafe_alloc.rs
+
+C:\Users\arter\Documents\Projects\WASM\nandemonaiya\target\wasm32-unknown-unknown\release\deps\buddy_alloc-8082f03ef3a5359a.d: C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\lib.rs C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\buddy_alloc.rs C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\fast_alloc.rs C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\non_threadsafe_alloc.rs
+
+C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\lib.rs:
+C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\buddy_alloc.rs:
+C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\fast_alloc.rs:
+C:\Users\arter\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\buddy-alloc-0.4.2\src\non_threadsafe_alloc.rs:
